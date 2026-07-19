89a64c03c06cf43c9395354e2861e261ddeb41c7
Salin Bade <102204614+SalinBade35@users.noreply.github.com>
2026-07-19 22:23:10 +0545

    Add files via upload

---
+++ day1/f1.py
@@ -0,0 +1,39 @@
+""" 
+# multi-line comment : ignore multiline of code
+single comment: #
+
+# .txt : text FileExistsError
+# .jpeg/.png : photo
+# .py: python file
+
+print("HELLO CLASS")
+
+print("we are learning python")
+
+
+
+
+
+# syntax: ?
+
+print('message')
+print("message")
+print("""message""")
+print('''message''')
+
+
+# comment: explain/ describe/ ignore the code
+
+
+# ctril + ~ : terminal 
+
+# python f1.py
+
+# indentation: space/ tab
+
+# python suite: block of code
+
+print("this is line 1")
+    
+print("this is line 1")
+"""
---
+++ day1/variables.py
@@ -0,0 +1,48 @@
+# variable: 
+
+container1 = "apple"
+container2 = "banana"
+container3 = 10
+container4 = 9.7
+container5 = True
+
+print("apple")
+# print(container1)
+print("container1")
+
+
+# Naming Rule
+
+# 1. values are kept in RHS
+variable1 = 'values'
+
+variable_2 = 10
+
+# variable-3 = 20 
+
+""" 
+1. can't start with number
+2. can't use special character except underscore
+3. can't use space in variable name
+"""
+
+age = 20
+print(age) # 20
+age = 21
+print(age) # 21
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
